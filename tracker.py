083cc6f379ea1024e66a5069479ad5073ba45b55
Sumesh Premraj <sumeshpremraj@gmail.com>
2018-10-19 08:33:26 +0530

    Move methods outside main method

--- tracker.py
+++ tracker.py
@@ -45,69 +45,73 @@ logger.addHandler(handler)
 #         }
 
 
+def auth():
+    logger.info("Initiating Sheets authorization")
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    return service
+
+
+def get_data(spreadsheet_id='1mp0x8AV7cTM45BqHzQ5uO-ga9JN48OA-yfNedcsnRkQ',
+             ranges=['PR Sheet!A2:P4', 'PR Sheet!A6:P8', 'PR Sheet!A10:P12', 'PR Sheet!A14:P16']):
+    service = auth()
+
+    logger.info("Getting data from Sheets")
+    values = service.spreadsheets().values().batchGet(spreadsheetId=spreadsheet_id, ranges=ranges,
+                                                      majorDimension='ROWS', valueRenderOption='UNFORMATTED_VALUE',
+                                                      dateTimeRenderOption='FORMATTED_STRING').execute()
+    return values
+
+
+def plot_data(values):
+    stats = {}
+    for i in range(0, 4):
+        lift = ''
+        for rep, weight, date in zip(values['valueRanges'][i]['values'][0], values['valueRanges'][i]['values'][1],
+                                     values['valueRanges'][i]['values'][2]):
+            if weight == 'Rep max':
+                # First column contains Squat/Rep Max/Date as the data
+                # This is to get the name of the lift
+                lift = rep
+                logger.debug("Lift: " + lift)
+                stats[lift] = {}
+                continue
+
+            elif weight not in (0, ''):
+                if not all([rep, weight, date]):
+                    logger.info("Missing data in the column, check spreadsheet")
+                    continue
+                # print(rep, weight, date)
+
+                # Epley formula
+                rep_max = weight * (1 + rep / 30)
+                logger.debug(date + " " + str(rep_max))
+
+                # TODO: Convert string to Python date objects OR check Sheets API for Date type
+                stats[lift][date] = rep_max
+
+        logger.info("Plotting " + lift)
+        x = OrderedDict(sorted(stats[lift].items(), key=lambda t: t[0]))
+        logger.debug(x)
+        plt.plot(x.keys(), x.values())  # (x-axis: date, y-axis: 1RM)
+
+    plt.show()
+    logger.debug("Stats: ")
+    logger.debug(stats)
+
+
 def main():
-    def auth():
-        logger.info("Initiating Sheets authorization")
-        store = file.Storage('token.json')
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-            creds = tools.run_flow(flow, store)
-        service = build('sheets', 'v4', http=creds.authorize(Http()))
-        return service
-
-
-    def get_data(spreadsheet_id='1mp0x8AV7cTM45BqHzQ5uO-ga9JN48OA-yfNedcsnRkQ', ranges=['PR Sheet!A2:P4', 'PR Sheet!A6:P8', 'PR Sheet!A10:P12', 'PR Sheet!A14:P16']):
-        service = auth()
-
-        logger.info("Getting data from Sheets")
-        values = service.spreadsheets().values().batchGet(spreadsheetId=spreadsheet_id,ranges=ranges,majorDimension='ROWS',valueRenderOption='UNFORMATTED_VALUE',dateTimeRenderOption='FORMATTED_STRING').execute()
-        return values
-
     values = get_data()
     if not values:
         logger.info('No data found.')
     else:
-        stats = {}
-
         plt.xlabel("Date")
         plt.ylabel("1RM")
-
-        def plot_data():
-            for i in range(0, 4):
-                lift = ''
-                for rep, weight, date in zip(values['valueRanges'][i]['values'][0], values['valueRanges'][i]['values'][1],
-                                             values['valueRanges'][i]['values'][2]):
-                    if weight == 'Rep max':
-                        # First column contains Squat/Rep Max/Date as the data
-                        # This is to get the name of the lift
-                        lift = rep
-                        logger.debug("Lift: " + lift)
-                        stats[lift] = {}
-                        continue
-
-                    elif weight not in(0, ''):
-                        if not all([rep, weight, date]):
-                            logger.info("Missing data in the column, check spreadsheet")
-                            continue
-                        # print(rep, weight, date)
-
-                        # Epley formula
-                        rep_max = weight * (1 + rep / 30)
-                        logger.debug(date + " " + str(rep_max))
-
-                        # TODO: Convert string to Python date objects OR check Sheets API for Date type
-                        stats[lift][date] = rep_max
-
-                logger.info("Plotting " + lift)
-                x = OrderedDict(sorted(stats[lift].items(), key=lambda t: t[0]))
-                logger.debug(x)
-                plt.plot(x.keys(), x.values())        # (x-axis: date, y-axis: 1RM)
-
-            plt.show()
-            logger.debug("Stats: ")
-            logger.debug(stats)
-        plot_data()
+        plot_data(values)
 
 
 if __name__ == '__main__':
